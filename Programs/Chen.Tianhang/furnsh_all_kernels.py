4ec70336513779fdb9e68c1f56281081a4df06fe
thxch <97776963+thxch@users.noreply.github.com>
2022-03-25 09:50:06 +0800

    Python file imported in Examine_01 code

---
+++ Programs/Chen.Tianhang/furnsh_all_kernels.py
@@ -0,0 +1,18 @@
+import spiceypy as spice
+
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_v300.tf')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/'
+             'PSP_kernels/spp_2018_224_2025_243_RO5_00_nocontact.alp.bc')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_001.tf')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_dyn_v201.tf')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_wispr_v002.ti')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_sclk_0865.tsc')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/naif0012.tls')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/earthstns_itrf93_201023.bsp')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_recon_20190120_20190416_v001.bsp')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_recon_20190416_20190914_v001.bsp')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_recon_20200101_20200301_v001.bsp')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_recon_20200802_20201016_v001.bsp')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_recon_20201016_20210101_v001.bsp')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/spp_recon_20210101_20210226_v001.bsp')
+spice.furnsh('D://Microsoft Download/Formal Files/data file/Kernels/PSP_kernels/pck00010.tpc')
